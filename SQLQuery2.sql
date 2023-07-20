a4a65c51d695ec21230834326594c98480f768e8
i-am-Melikov <129064718+i-am-Melikov@users.noreply.github.com>
2023-07-20 14:46:55 +0400

    Add files via upload

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -1,6 +1,7 @@
 CREATE TABLE Workers(
 Id INT PRIMARY KEY IDENTITY,
 Fullname NVARCHAR(30),
+Img NVARCHAR(30),
 ProfessionsId INT FOREIGN KEY REFERENCES Professions(Id))
 
 
@@ -10,14 +11,16 @@ Id INT PRIMARY KEY IDENTITY,
 )
 
 INSERT INTO Professions([Name])
+VALUES ('Manager')
 VALUES	('Florist')
---VALUES ('Manager')
 
-INSERT INTO Workers(Fullname,ProfessionsId)
-VALUES ('AMANDA WATKINS',1)
-VALUES ('BEVERLY CLARK',1)
-VALUES ('SHIRLEY HARRIS',2)
-VALUES ('CRYSTAL BROOKS',1)
+INSERT INTO Workers(Fullname, Img, ProfessionsId)
+VALUES ('CRYSTAL BROOKS','image4',1)
+VALUES ('SHIRLEY HARRIS','image3',2)
+VALUES ('BEVERLY CLARK','image2',1)
+VALUES ('AMANDA WATKINS','image1',1)
+
+
 
 
 
